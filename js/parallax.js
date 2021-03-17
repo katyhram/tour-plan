4a3f80efbe6b5d347d9919ca6b0880cea8653c38
katyhram <katy.khramchenko@gmail.com>
2021-03-17 14:34:54 +0300

    create: created footer-lists in html, added parallax

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,13 @@
-$('.parallax-window').parallax({
-  imageSrc: '../img/newsletter-bg.jpg',
-  speed: 0.2,
-  zIndex: -100
-});
+var scrolled = $(window).scrollTop()
+$('.parallax-window').each(function(index) {
+var imageSrc = $(this).data('../img/newsletter-bg.jpg')
+var imageHeight = $(this).data('1913px')
+$(this).css('background-image','url(' + imageSrc + ')')
+$(this).css('height', imageHeight)
+ 
+var initY = $(this).offset().top
+var height = $(this).height()
+var diff = scrolled - initY
+var ratio = Math.round((diff / height) * 100)
+$(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
+})
