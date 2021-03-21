a289ad1328a3480b3c260c0a0f8db41a35563d93
katyhram <katy.khramchenko@gmail.com>
2021-03-22 01:37:52 +0300

    create: created active forms on pages

---
+++ thankyou.php
@@ -0,0 +1,20 @@
+<?php include('header.php')?>
+  
+  <section class="hotel">
+    <div class="container">
+      <div class="hotel-info">
+        <div class="hotel-info__text">
+          <div class="hotel-wrapper">
+            <h1 class="hotel-name hotel-info__name">Сообщение отправлено!</h1>
+          </div>
+          <p class="hotel-description hotel-info__description">Наш менеджер свяжется с Вами в ближайшее время</p>
+        </div>
+        <!-- ./hotel-info__text -->
+
+      </div>
+      <!-- hotel-info -->
+    </div>
+    <!-- /.container -->
+  </section>
+  <!-- ./hotel -->
+<?php include('footer.php')?>
